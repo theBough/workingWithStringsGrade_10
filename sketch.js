99cf406eb51245c7905538b6149a0301cf54239f
corbin Boughen <50217007+theBough@users.noreply.github.com>
2026-04-07 09:14:39 -0500

    Enhance string processing to include uppercase first letter
    Added functionality to display the first letter of the input word in uppercase.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
 //Global Variables
-let myInput, myButton,stringLength;
-
-//----------------------------
+let myInput, myButton,stringLength,allCaps;
+function stringStuff(){
+  let theirWord = myInput.value()//local variable used to grab their Word
+  stringLength = theirWord.length;
+  allCaps =  theirWord.charAt(0).toUpperCase();
+}
+//--PreBuilt p5js functions--------------
 function setup() {
   createCanvas(400,400);
   myInput = createInput("Word");
@@ -10,14 +14,13 @@ function setup() {
   myButton.position(100,100);
   myButton.mousePressed(stringStuff)
   stringLength = "";
+  allCaps = "";
   
 }
 function draw() {
   background(220);
   text("Work with Strings", 10,10)
   text("The length of your word is: " + stringLength,20,60)
+  text("Your word all caps is : " + allCaps,20,80)
 }
 //---------------------------------------
-function stringStuff(){
-  stringLength = myInput.value().length
-}
