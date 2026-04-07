0f26214d73df1917b4a1b661baff59a66e526e60
corbin Boughen <50217007+theBough@users.noreply.github.com>
2026-04-07 08:56:49 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,23 +1,23 @@
 //Global Variables
-let myInput, myButton;
+let myInput, myButton,stringLength;
 
 //----------------------------
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400,400);
   myInput = createInput("Word");
   myButton = createButton("Press");
   myInput.position(20,20)
   myButton.position(100,100);
   myButton.mousePressed(stringStuff)
+  stringLength = "";
   
 }
 function draw() {
   background(220);
   text("Work with Strings", 10,10)
-  
-  
+  text("The length of your word is: " + stringLength,20,60)
 }
 //---------------------------------------
 function stringStuff(){
-  
+  console.log("hi")
 }
