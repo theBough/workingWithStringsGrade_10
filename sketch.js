7f2754e90792912bbb5a96114ce572bbf07ac280
corbin Boughen <50217007+theBough@users.noreply.github.com>
2026-04-07 08:45:31 -0500

    Initialize input and button in setup function
    Added input and button elements for user interaction.

--- sketch.js
+++ sketch.js
@@ -1,8 +1,23 @@
+//Global Variables
+let myInput, myButton;
+
+//----------------------------
 function setup() {
   createCanvas(400, 400);
+  myInput = createInput("Word");
+  myButton = createButton("Press");
+  myInput.position(20,20)
+  myButton.position(100,100);
+  myButton.mousePressed(stringStuff)
+  
 }
-
 function draw() {
   background(220);
   text("Work with Strings", 10,10)
+  
+  
+}
+//---------------------------------------
+function stringStuff(){
+  
 }
