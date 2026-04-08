e972e148d2ed3414e7fe8629c505f21bdb61db11
corbin Boughen <50217007+theBough@users.noreply.github.com>
2026-04-08 09:35:35 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,21 @@
 //Global Variables
 let myInput, myButton,stringLength,allCaps;
+
+function getVowels(theirW){
+  //thierW has a scope that is local to this function
+  //meaning only this function can see it or use it.
+  
+  let vowelList = ""
+  if(theirW.indexOf('a')  != -1){
+    //if a is in their word, this will be true.
+    vowelList += 'a, '
+  }
+  if(theirW.indexOf('e')  != -1){
+    //if a is in their word, this will be true.
+    vowelList += 'e, '
+  }
+  return vowelList
+}
 function stringStuff(){
   let theirWord = myInput.value()//local variable used to grab their Word
   stringLength = theirWord.length;
@@ -26,6 +42,7 @@ function draw() {
   text("Work with Strings", 10,10)
   text("The length of your word is: " + stringLength,20,60)
   text("Your word all caps is : " + allCaps,20,80)
+  text("Your word contains the following vowels: " + getVowels(myInput.value()),20,100 )
   //add another text to output all lowercase
 }
 //---------------------------------------
